8f852780f375b5a2eb93e8f651462f84a1d9fb50
Colin <colin.beauvisage@gmail.com>
2021-10-01 00:05:12 +0200

    wip

--- app/src/main/java/com/example/jaken/PagePremierNiveau.java
+++ app/src/main/java/com/example/jaken/PagePremierNiveau.java
@@ -3,12 +3,22 @@ package com.example.jaken;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class PagePremierNiveau extends AppCompatActivity {
+    ImageButton btnImage1;
+    Jaken jaken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page_premier_niveau);
+        jaken = new Jaken(getApplicationContext(), 1);
+
+        btnImage1 = findViewById(R.id.btnImage1);
+
+        btnImage1.setOnClickListener(v-> {
+            jaken.play(1);
+        });
     }
 }
