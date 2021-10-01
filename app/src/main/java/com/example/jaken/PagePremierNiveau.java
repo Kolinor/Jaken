516a8aaa3990ceddbd4d18cece8d6cb86f500339
Colin <colin.beauvisage@gmail.com>
2021-10-01 09:34:40 +0200

    jaken

--- app/src/main/java/com/example/jaken/PagePremierNiveau.java
+++ app/src/main/java/com/example/jaken/PagePremierNiveau.java
@@ -3,10 +3,16 @@ package com.example.jaken;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class PagePremierNiveau extends AppCompatActivity {
     ImageButton btnCiseaux;
+    ImageButton btnFeuille;
+    ImageButton btnPierre;
+    TextView textViewNumeroManche;
+    TextView textViewTour;
     Jaken jaken;
 
     @Override
@@ -16,16 +22,70 @@ public class PagePremierNiveau extends AppCompatActivity {
         jaken = new Jaken(getApplicationContext(), 1);
 
         btnCiseaux = findViewById(R.id.btnCiseaux);
+        btnFeuille = findViewById(R.id.btnFeuille);
+        btnPierre = findViewById(R.id.btnPierre);
+        textViewNumeroManche = findViewById(R.id.textViewNumeroManche);
+        textViewTour = findViewById(R.id.textViewTour);
 
         btnCiseaux.setOnClickListener(v-> {
-            switch (jaken.play(Signe.ciseaux.getValue())) {
-                case -1:
-                    break;
-                case 0:
-                    break;
-                case 1:
-                    break;
-            }
+            gestionTour(btnCiseaux, jaken.play(Signe.ciseaux.getValue()));
+
+            setNumeroManche();
+            clearColors();
         });
+
+        btnFeuille.setOnClickListener(v-> {
+            gestionTour(btnFeuille, jaken.play(Signe.feuille.getValue()));
+
+            setNumeroManche();
+            clearColors();
+        });
+
+        btnPierre.setOnClickListener(v-> {
+            gestionTour(btnPierre, jaken.play(Signe.pierre.getValue()));
+
+            setNumeroManche();
+            clearColors();
+        });
+    }
+
+    public void setNumeroManche() {
+        int numManche = jaken.getManches();
+        textViewNumeroManche.setText(String.valueOf(numManche));
+
+        if (numManche < 6) return;
+
+        // cas de victoire
+    }
+
+    public void gestionTour(ImageButton btn, int value) {
+        switch (value) {
+            case -1:
+                System.out.println("défaite");
+                btn.setBackgroundColor(getColor(R.color.red));
+                break;
+            case 0:
+                System.out.println("égalité");
+                btn.setBackgroundColor(getColor(R.color.black));
+                break;
+            case 1:
+                System.out.println("victoire");
+                btn.setBackgroundColor(getColor(R.color.green));
+                break;
+        }
+    }
+
+    private void clearColors() {
+        new CountDownTimer(2000, 1000) {
+                public void onTick(long millisUntilFinished) {}
+
+                public void onFinish() {
+                    btnCiseaux.setBackgroundColor(getColor(R.color.white));
+                    btnPierre.setBackgroundColor(getColor(R.color.white));
+                    btnFeuille.setBackgroundColor(getColor(R.color.white));
+
+                    System.out.println("fini");
+                }
+            }.start();
     }
 }
